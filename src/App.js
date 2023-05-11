f69e4277f1777549759f6f5911e84a71481e4a0d
Justice Uzoma Ajah <77506031+J-Ajah@users.noreply.github.com>
2023-05-11 18:17:28 +0000

    Added a messenger route and an error page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ import {
 } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
+import Messenger from "./pages/messenger/Messenger";
 
 function App() {
   const { user } = useContext(AuthContext);
+
+  const errorMessage = <div className="flex items-center justify-center h-[100vh] text-2xl">Sorry, page not available</div>
   return (
     <Router>
       <Routes>
@@ -23,7 +26,10 @@ function App() {
           element={user ? <Navigate to="/" /> : <Login />}
         ></Route>
         <Route path={"/register"} element={user ? <Navigate to="/"/>:<Register />}></Route>
+        <Route path={"/messenger"} element={!user ? <Navigate to="/"/>:<Messenger />}></Route>
         <Route path={"/profile/:username"} element={<Profile />}></Route>
+        <Route path={"*"} element={errorMessage}></Route>
+
       </Routes>
     </Router>
   );
